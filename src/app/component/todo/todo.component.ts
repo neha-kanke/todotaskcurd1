d69f4e48d4a9c0887052eea987824927dcb86e61
neha-kanke <nehakanke95@gmail.com>
2025-12-27 18:22:05 +0530

    todocurd1

---
+++ src/app/component/todo/todo.component.ts
@@ -0,0 +1,59 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Itodo } from 'src/app/model/todo.model';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.sass']
+})
+export class TodoComponent implements OnInit {
+  editId !: string
+  iseditbrn: boolean = false
+  // newtodoarr: Array<Itodo> = []
+  todoarr: Array<Itodo> = [{
+    todoName: "neha",
+    todoId: '123'
+  }]
+  @ViewChild('todoelemnet') todoelemnet !: ElementRef
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onaddbtn() {
+    if(this.todoelemnet.nativeElement.value){
+    let newobj = {
+      todoName: this.todoelemnet.nativeElement.value,
+      todoId: Date.now().toString()
+
+    }
+    this.todoarr.push(newobj)
+    
+    }
+this.todoelemnet.nativeElement.value = ""
+  }
+
+  oneditbtn(todo: Itodo) {
+    this.todoelemnet.nativeElement.value = todo.todoName
+    this.editId = todo.todoId
+    this.iseditbrn = true
+  }
+
+  onupdatebtn() {
+
+    let updateobj = {
+      todoName: this.todoelemnet.nativeElement.value,
+      todoId: this.editId
+    }
+    let findindex = this.todoarr.findIndex(todo => todo.todoId === updateobj.todoId)
+    this.todoarr[findindex] = updateobj
+    this.iseditbrn = false
+    this.todoelemnet.nativeElement.value = ""
+
+  }
+
+
+  ondelete(i: number) {
+    this.todoarr.splice(i)
+  }
+}
