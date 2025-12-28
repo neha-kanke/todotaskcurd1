60301c0d82ae07eb3da7cc868dc90ca1823d6234
neha-kanke <nehakanke95@gmail.com>
2025-12-28 17:53:58 +0530

    update all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TodoComponent } from './component/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/todo.model.ts
@@ -0,0 +1,6 @@
+
+
+export interface Itodo{
+    todoName:string
+    todoId:string
+}
